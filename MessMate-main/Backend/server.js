60a065dcdb81d9705becb2ca4f73d7bc651f9f48
TheBadDevVed <jvedant578@gmail.com>
2025-11-25 00:15:25 +0530

    messMate

---
+++ MessMate-main/Backend/server.js
@@ -0,0 +1,29 @@
+  require("dotenv").config();
+  const express = require("express");
+  const cors = require("cors");
+  const authRoutes = require("./routes/authRoutes");
+  const dashboardRoutes = require("./routes/dashboardRoutes");
+  const expenseRoutes = require("./routes/expenseRoutes");
+  const menuRoutes = require("./routes/menuRoutes");
+  // const profileRoutes = require("./routes/profileRoutes");
+  const feedbackRoutes = require("./routes/feedbackRoutes");
+  const adminRoutes = require("./routes/adminRoutes");
+
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+
+  // ✅ Routes
+  app.use("/", authRoutes);
+  app.use("/dashboard", dashboardRoutes);
+  app.use("/", menuRoutes);
+  app.use("/feedback", feedbackRoutes);
+  app.use("/",adminRoutes);
+  app.use("/expense", expenseRoutes);
+
+  // app.use("/", profileRoutes);
+  // ✅ Start Server
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
